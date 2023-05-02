fa832ad2d3229d537670df0d7b7ab90250843663
Matt <mjstilling@gmail.com>
2023-05-02 17:25:10 -0500

    Updates to client.c and server.c

--- client.c
+++ client.c
@@ -13,7 +13,33 @@ struct rpc_connection RPC_init(int src_port, int dst_port, char dst_addr[])
 // Sleeps the server thread for a few seconds
 void RPC_idle(struct rpc_connection *rpc, int time)
 {
+    // first pack identifier for desired function (idle), args, client id, and seq_number into payload
+    // probably use a struct to organize this
 
+    // then send packet with this payload
+    // send_packet(rpc->recv_socket, rpc->dst_addr, rpc->dst_addr, );
+    
+    int num_retries = 0;
+    struct packet_info packet;
+    while (num_retries <= 5) {
+        packet = receive_packet_timeout(rpc->recv_socket, 1); // 1s timeout
+        if (packet.recv_len != 0) {
+            if (0) { // TODO change this to if ACK recieved
+                sleep(1);
+                num_retries = 0;
+                continue;
+            } else if (0) { // TODO change this to if packet has other client ID or old seq number
+                continue;
+            } else { // correct packet recieived
+                break;
+            }
+        }
+        num_retries++;
+    }
+    if (num_retries > 5) {
+        printf("Retry failed");
+        exit(1);
+    }
 }
 
 // gets the value of a key on the server store
--- server.c
+++ server.c
@@ -1,6 +1,29 @@
 #include "server_functions.h"
+#include "udp.h"
 
-int main()
+struct client {
+    int client_id;
+    int last_seq_number;
+    int last_result;
+};
+
+int main(int argc, char **argv)
 {
-    
+    if (argc != 2) {
+        printf("expected usage: ./server <port>\n");
+        exit(0);
+    }
+    struct socket s = init_socket(argv[1]);
+
+    struct client call_table[100];
+    int num_clients = 0;
+
+    while (1) {
+        struct packet_info packet = receive_packet(s);
+        for (int i = 0; i < num_clients; i++) {
+
+        }
+
+        break;
+    }
 }
